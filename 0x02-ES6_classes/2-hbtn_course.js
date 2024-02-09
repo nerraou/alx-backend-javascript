57026ebe69e7ea145c6e0afe2b5afc34d3e04908
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-09 01:56:39 +0100

    fix eslant errors

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -21,7 +21,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof length != 'number') {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
@@ -33,8 +33,8 @@ export default class HolbertonCourse {
 
   set students(students) {
     if (
-      !(students instanceof Array)||
-      !students.every((student) => typeof student === 'string')
+      !(students instanceof Array)
+      || !students.every((student) => typeof student === 'string')
     ) {
       throw new TypeError('Students must be an array of strings');
     }

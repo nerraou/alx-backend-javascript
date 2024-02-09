7593d621b6016081e791ac52a42e3256c97ecc2e
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-09 01:10:05 +0100

    fix: single quote

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -10,8 +10,8 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof name !== "string") {
-      throw new TypeError("Name must be a string");
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
@@ -21,8 +21,8 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof length != "number") {
-      throw new TypeError("Length must be a number");
+    if (typeof length != 'number') {
+      throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
@@ -34,9 +34,9 @@ export default class HolbertonCourse {
   set students(students) {
     if (
       !Array.isArray(students) ||
-      !students.every((student) => typeof student === "string")
+      !students.every((student) => typeof student === 'string')
     ) {
-      throw new TypeError("Students must be an array of strings");
+      throw new TypeError('Students must be an array of strings');
     }
     this._students = students;
   }

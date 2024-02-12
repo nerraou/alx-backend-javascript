217303112d865789486bb61546a38dde1db53758
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-12 23:42:36 +0100

    check for type

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) return '';
+  if (startString.length === 0 || typeof startString !== 'string') return '';
   const strings = [];
 
   set.forEach((value) => {

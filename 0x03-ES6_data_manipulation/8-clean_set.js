809cad7fcf865d4c09f06f03d1bab8ed7de4da5e
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-12 23:50:41 +0100

    make changes

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || typeof startString !== 'string' || !set.instanceof(Set)) return '';
   const strings = [];
 
+  if (startString.length === 0 || typeof startString !== 'string') return '';
   set.forEach((value) => {
     if (typeof value === 'string' && value.startsWith(startString)) {
-      strings.push(value.substring(startString.length));
+      strings.push(value.slice(startString.length));
     }
   });
   return strings.join('-');

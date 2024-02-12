a07cab0ba244f4cd8c6f0dc6a47e7eaa24fa3643
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-12 17:29:14 +0100

    filter array

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,4 @@
+export default function getStudentsByLocation(studentList, city) {
+  if (!Array.isArray(studentList)) return [];
+  return studentList.filter((student) => student.location === city);
+}

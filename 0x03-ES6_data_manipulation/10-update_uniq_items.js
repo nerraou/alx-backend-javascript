143e91b14f4d85a301687fc6c00b2ec6fb407a05
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-13 12:54:01 +0100

    fix typo error

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,7 +3,9 @@ export default function updateUniqueItems(map) {
     throw new Error('Cannot process');
   }
   map.forEach((value, key, newMap) => {
-    if (value === 1) newMap.set(key, 100);
+    if (value === 1) {
+      newMap.set(key, 100);
+    }
   });
 
   return map;

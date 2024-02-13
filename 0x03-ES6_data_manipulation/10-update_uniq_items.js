60cf8fadc03a5a28c9aa63fb5124cedd7df95552
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-13 12:49:32 +0100

    update uniq items

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map) || map.size === 0) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key, newMap) => {
+    if (value === 1) newMap.set(key, 100);
+  });
+
+  return map;
+}

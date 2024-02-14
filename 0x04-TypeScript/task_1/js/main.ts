8a8f2dc5396b5b2060bd32548fb5f2ecb55b7a2d
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-15 00:07:41 +0100

    create print teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,3 +17,7 @@ const teacher3: Teacher = {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+function printTeacher(firstName: string, lastName: string) {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}

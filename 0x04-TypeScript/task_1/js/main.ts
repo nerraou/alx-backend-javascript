ab60e2e5f7b689ef8ef20ff8601fc1d0796e1413
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-15 00:36:38 +0100

    interface class  implement interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -21,3 +21,28 @@ interface Directors extends Teacher {
 function printTeacher(firstName: string, lastName: string) {
   return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+interface IStudentClass {
+  firstName: string;
+  lastName: string;
+  workOnHomework: () => string;
+  displayName: () => string;
+}
+
+class StudentClass implements IStudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return "Currently working";
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}

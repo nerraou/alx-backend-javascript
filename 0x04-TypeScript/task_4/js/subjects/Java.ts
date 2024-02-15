ed29ac28f6fdb02da419454d02f995cbf2ce8cc9
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-15 01:28:14 +0100

    create task4

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,16 @@
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+  export class Java extends Subject {
+    getRequirements() {
+      return "Here is the list of requirements for Java";
+    }
+    getAvailableTeacher() {
+      if (this.teacher.experienceTeachingC == 0) {
+        return "No available teacher";
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
